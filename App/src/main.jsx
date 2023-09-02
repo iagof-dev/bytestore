150fb585b38810a72cf6c05d962a21c3ee2e5bea
Iago F <n3rdydzn@hotmail.com>
2023-09-02 18:44:37 -0300

    React Project

---
+++ App/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './Assets/css/tailwind.css';
+import App from './app.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App/>,
+);
---
+++ App/src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Menu from '../Components/menu.jsx';
+
+export default function login() {
+  return (
+    <div>
+        <Menu />
+        <h1>login page</h1>
+    </div>
+  )
+}
