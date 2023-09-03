949870dbfb5af257b4daaf8ecf5ad1d519e67985
Iago F <n3rdydzn@hotmail.com>
2023-09-02 23:49:46 -0300

    Finishing Card

--- App/src/pages/home.jsx
+++ App/src/pages/home.jsx
@@ -16,69 +16,81 @@ const Home = () => {
 
 
               <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                <div className='p-2'>
-                  <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                  <div className='container'>
-                    <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                    <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                    <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                <a href='/anuncio/x'>
+                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
+                  <div className='p-2'>
+                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
+                    <div className='container'>
+                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
+                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
+                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                    </div>
                   </div>
-                </div>
-                <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                </a>
               </div>
 
               <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                <div className='p-2'>
-                  <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                  <div className='container'>
-                    <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                    <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                    <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                <a href='/anuncio/x'>
+                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
+                  <div className='p-2'>
+                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
+                    <div className='container'>
+                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
+                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
+                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                    </div>
                   </div>
-                </div>
-                <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                </a>
               </div>
 
               <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                <div className='p-2'>
-                  <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                  <div className='container'>
-                    <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                    <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                    <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                <a href='/anuncio/x'>
+                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
+                  <div className='p-2'>
+                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
+                    <div className='container'>
+                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
+                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
+                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                    </div>
                   </div>
-                </div>
-                <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                </a>
               </div>
 
               <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                <div className='p-2'>
-                  <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                  <div className='container'>
-                    <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                    <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                    <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                <a href='/anuncio/x'>
+                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
+                  <div className='p-2'>
+                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
+                    <div className='container'>
+                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
+                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
+                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                    </div>
                   </div>
-                </div>
-                <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                </a>
               </div>
 
               <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                <div className='p-2'>
-                  <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                  <div className='container'>
-                    <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                    <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                    <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                <a href='/anuncio/x'>
+                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
+                  <div className='p-2'>
+                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
+                    <div className='container'>
+                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
+                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
+                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
+                    </div>
                   </div>
-                </div>
-                <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
+                </a>
               </div>
+
+              
 
 
             </div>
