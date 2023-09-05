8941c09339ced7829ed6b45fd5fbf6351c0dca9e
Iago F <n3rdydzn@hotmail.com>
2023-09-04 21:43:18 -0300

    Some adjusts

--- App/src/pages/home.jsx
+++ App/src/pages/home.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Menu from '../Components/menu.jsx';
-import teste from '../Assets/img/placeholder.png';
-import { FaCartShopping } from "react-icons/fa6";
-
+import Card from '../props/card';
 
 const Home = () => {
   return (
@@ -10,89 +8,14 @@ const Home = () => {
       <Menu />
 
       <div className='md:container mx-auto w-[50%]'>
-        <div className='w-full h-5 mx-auto bg-purple-900'>
-          <div className="md:container w-full mt-1 bg-pink-600 flex flex-grow justify-center">
-            <div className='grid grid-rows-1 grid-cols-5 grid-flow-col gap-5 items-center justify-center '>
-
-
-              <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <a href='/anuncio/x'>
-                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                  <div className='p-2'>
-                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                    <div className='container'>
-                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
-                    </div>
-                  </div>
-                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
-                </a>
-              </div>
-
-              <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <a href='/anuncio/x'>
-                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                  <div className='p-2'>
-                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                    <div className='container'>
-                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
-                    </div>
-                  </div>
-                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
-                </a>
-              </div>
-
-              <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <a href='/anuncio/x'>
-                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                  <div className='p-2'>
-                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                    <div className='container'>
-                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
-                    </div>
-                  </div>
-                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
-                </a>
-              </div>
-
-              <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <a href='/anuncio/x'>
-                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                  <div className='p-2'>
-                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                    <div className='container'>
-                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
-                    </div>
-                  </div>
-                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
-                </a>
-              </div>
-
-              <div className=' rounded-md shadow-2xl w-[11rem] bg-gray-200'>
-                <a href='/anuncio/x'>
-                  <img src={teste} alt='aaaaaa' className='rounded-t-md' title='aaaaa' />
-                  <div className='p-2'>
-                    <h1 className='text-[0.75rem]'>Processador Intel Core i7 12700KF 3.6GHz (5.0GHz Turbo), 12ª Geração, 12-Cores 20-Threads, LGA 1700</h1>
-                    <div className='container'>
-                      <h3 className='text-gray-500 text-[0.8rem] pt-5 mb-[-0.4rem]'><span className='line-through'>de: R$1.000</span> por:</h3>
-                      <h2 className='text-green-500 pb-[-2rem] font-bold'>R$1.500,00 à vista</h2>
-                      <h4 className='text-gray-600 text-[0.8rem] mt-[-0.5rem]'>96x de R$15,63 sem juros</h4>
-                    </div>
-                  </div>
-                  <a href='/anuncio/x'><button className=' bg-[#00A000] rounded-sm text-white w-full h-10 rounded-b-md'>Adicionar ao carrinho</button></a>
-                </a>
-              </div>
-
-              
-
-
+        <div className='w-full h-5 mx-auto'>
+          <div className="md:container w-full mt-1 flex flex-grow justify-center">
+            <div className='grid grid-rows-1 grid-cols-5 gap-5 items-center justify-center '>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
             </div>
           </div>
         </div>
