14db371625b0aac693832b58612df7cf2438f795
Iago "N3rdy" Fragnan <n3rdydzn@hotmail.com>
2023-09-10 20:02:07 -0300

    Delete App directory

--- App/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './Assets/css/tailwind.css';
-import App from './app.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>,
-);
